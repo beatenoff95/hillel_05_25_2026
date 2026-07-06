55bebb684a1c5849a7b902a074b9781186d69ce9
yevhenii.vyshnevskyi@trxservices.com <yevhenii.vyshnevskyi@trxservices.com>
2026-07-06 13:05:55 +0300

    Fix TeamLead parent initialization

--- lesson_10/homework_10.py
+++ lesson_10/homework_10.py
@@ -10,21 +10,20 @@ class Employee:
 
 class Manager(Employee):
     def __init__(self, name, salary, department):
-        super().__init__(name, salary)
+        Employee.__init__(self, name, salary)
         self.department = department
 
 
 class Developer(Employee):
     def __init__(self, name, salary, programming_language):
-        super().__init__(name, salary)
+        Employee.__init__(self, name, salary)
         self.programming_language = programming_language
 
 
 class TeamLead(Manager, Developer):
     def __init__(self, name, salary, department, programming_language, team_size):
-        Employee.__init__(self, name, salary)
-        self.department = department
-        self.programming_language = programming_language
+        Manager.__init__(self, name, salary, department)
+        Developer.__init__(self, name, salary, programming_language)
         self.team_size = team_size
 
 
